a208404075dab539da6eb4a5232da4cff2d385a7
Bhavitha M <b44068845@gmail.com>
2024-08-06 00:44:01 +0530

    Media Queries!!

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    host: "0.0.0.0", // Allows access from local network
+    port: 80, // Use port 80
+  },
   base: "/bhavitha/",
 });
