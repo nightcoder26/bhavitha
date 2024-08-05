43dae5bf073165e03760ff4fa0a63879b42e851d
Bhavitha M <b44068845@gmail.com>
2024-08-05 18:38:09 +0530

    Apply smooth scroll

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Enables access via local IP
-  },
+
+  base: "/bhavitha/",
 });
