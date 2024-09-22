7612771b27054fd2588f0f05b3ce24faaf3d7875
Bhavitha M <b44068845@gmail.com>
2024-09-23 02:16:19 +0530

    fix little issues with cursor

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,20 @@ const App = () => {
 
   return (
     <>
-      <AnimatedCursor />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color-2)",
+        }}
+      />
       <div className="main-container">
         <nav className={`nav ${showNav ? "visible" : "hidden"}`}>
           <ul>
